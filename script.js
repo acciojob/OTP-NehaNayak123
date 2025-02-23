5da10cd4f24f34e593592b342e8ca6be8a102c80
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-02-23 15:40:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+        inputs.forEach((input, index) => {
+            input.addEventListener('input', (e) => {
+                // Ensure only digits are entered
+                if (e.target.value.match(/\d/)) {
+                    if (index < inputs.length - 1) {
+                        inputs[index + 1].focus();
+                    }
+                } else {
+                    e.target.value = '';
+                }
+            });
+
+            input.addEventListener('keydown', (e) => {
+                if (e.key === 'Backspace') {
+                    if (!e.target.value && index > 0) {
+                        inputs[index - 1].focus();
+                    }
+                }
+            });
+        });
